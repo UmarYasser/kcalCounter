c205d3b01d9eb2f814f629d12551d253b99c6b33
UmarYasser <110424876+UmarYasser@users.noreply.github.com>
2026-02-02 17:08:20 +0200

    Refactor file path resolution for templates

--- Routes/pagesRouter.js
+++ Routes/pagesRouter.js
@@ -1,18 +1,18 @@
 const fs = require('fs')
 const path = require('path')
-const authCon = require('./../Controllers/authController')
-const Home = fs.readFileSync('./Public/template/Home.html');
-const SignUp = fs.readFileSync('./Public/template/SignUp.html');
-const LogIn = fs.readFileSync(path.join(__dirname,'Public/template/LogIn.html'))
-const SetUp = fs.readFileSync('./Public/template/SetUp.html')
+const authCon = require(path.join(__dirname,'..','Controllers/authController')
+const Home = fs.readFileSync(path.join(__dirname,'..','Public/template/Home.html');
+const SignUp = fs.readFileSync(path.join(__dirname,'..','Public/template/SignUp.html');
+const LogIn = fs.readFileSync(path.join(__dirname,'..', 'Public/template/LogIn.html'))
+const SetUp = fs.readFileSync( path.join(__dirname,'..','Public/template/SetUp.html')
 
-const Tracker = fs.readFileSync('./Public/template/Tracker.html')
-const Activity = fs.readFileSync('./Public/template/Activity.html')
-const Admin = fs.readFileSync('./Public/template/Admin.html')
-const notFound = fs.readFileSync('./Public/template/404.html')
-const forbidden = fs.readFileSync('./Public/template/403.html')
-const ResetPassword = fs.readFileSync('./Public/template/ResetPassword.html')
-const Me = fs.readFileSync('./Public/template/Me.html')
+const Tracker = fs.readFileSync(path.join(__dirname,'..','Public/template/Tracker.html')
+const Activity = fs.readFileSync(path.join(__dirname,'..','Public/template/Activity.html')
+const Admin = fs.readFileSync(path.join(__dirname,'..','Public/template/Admin.html')
+const notFound = fs.readFileSync(path.join(__dirname,'..','Public/template/404.html')
+const forbidden = fs.readFileSync(path.join(__dirname,'..','Public/template/403.html')
+const ResetPassword = fs.readFileSync(path.join(__dirname,'..','Public/template/ResetPassword.html')
+const Me = fs.readFileSync(path.join(__dirname,'..','Public/template/Me.html')
 
 const router = require('express').Router()
 
